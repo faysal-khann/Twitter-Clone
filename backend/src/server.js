7500e2d4b36138ad3cc1f3be8e99a9f9c2d5407d
MD. FAYSAL KHAN <22-49312-3@student.aiub.edu>
2026-08-03 23:41:46 +0600

    Implement database connection and define models for comments, notifications, posts, and users

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,13 @@
 import express from "express"
+import {ENV} from "./config/env.js"
+import {connectDB} from "./config/db.js"
 
 const app=express();
 
+connectDB();
 
-app.listen(5001,()=>console.log("Server is up and runnig on PORT:5001"))
+app.get("/",(req,res)=>{
+    res.send("Hello  from backend")
+})
+
+app.listen(ENV.PORT,()=>console.log("Server is up and runnig on ",ENV.PORT));
