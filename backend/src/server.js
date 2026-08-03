90092462c35336692197e8c216f07896d563f79b
MD. FAYSAL KHAN <22-49312-3@student.aiub.edu>
2026-08-04 00:00:01 +0600

    coderabbit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,18 @@
-import express from "express"
-import {ENV} from "./config/env.js"
-import {connectDB} from "./config/db.js"
+import express from "express";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
-const app=express();
+const app = express();
 
-connectDB();
+const startServer = async () => {
+  await connectDB();
+  app.listen(ENV.PORT, () =>
+    console.log("Server is up and runnig on ", ENV.PORT),
+  );
+};
 
-app.get("/",(req,res)=>{
-    res.send("Hello  from backend")
-})
+app.get("/", (req, res) => {
+  res.send("Hello  from backend");
+});
 
-app.listen(ENV.PORT,()=>console.log("Server is up and runnig on ",ENV.PORT));
+startServer();
